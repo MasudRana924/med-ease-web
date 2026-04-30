9aa53a24a18ee3e799ca849793116bc4ae855d52
MasudRana924 <masud15-924@diu.edu.bd>
2026-04-30 22:53:20 +0600

    first commit

--- lib/auth/actions.ts
+++ lib/auth/actions.ts
@@ -3,9 +3,11 @@ import { AuthResponse, LoginCredentials, SignupCredentials } from "@/types";
 
 export const AuthService = {
     login: async (credentials: LoginCredentials): Promise<AuthResponse> => {
-        return apiClient.post<AuthResponse>("/auth/login", credentials);
+        const response = await apiClient.post<AuthResponse>("/auth/login", credentials);
+        return response.data;
     },
     signup: async (data: SignupCredentials): Promise<AuthResponse> => {
-        return apiClient.post<AuthResponse>("/register", data);
+        const response = await apiClient.post<AuthResponse>("/register", data);
+        return response.data;
     }
 };
